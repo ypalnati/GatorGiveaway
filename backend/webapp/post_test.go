7354d5f1473f2491f0ae3e2763a772596c91b640
Raghuveer Sharma Saripalli <rvs1604@gmail.com>
2022-03-04 17:51:57 -0500

    Mock API test for Seller Read Pass case, Seller Create Post pass and fail case

---
+++ backend/webapp/post_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	m "webapp/model"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSellerReadPostPassCase(t *testing.T) {
+	login := m.Login{
+		Username: "testadmin",
+		Password: "TestAdmin@123",
+	}
+	payload, _ := json.Marshal(login)
+	nr := httptest.NewRecorder()
+	req1, _ := http.NewRequest("POST", "/login", strings.NewReader(string(payload)))
+	req1.Header.Set("Content-Type", "application/json")
+	req1.Header.Set("credentials", "include")
+	router.ServeHTTP(nr, req1)
+	cookieValue := nr.Result().Header.Get("Set-Cookie")
+	if nr.Code == 200 {
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/read", nil)
+		req1.Header.Set("credentials", "include")
+		req.Header.Set("Cookie", cookieValue)
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+		var userPosts []m.Product
+		for i := 0; i < len(posts); i++ {
+			if posts[i].UserId == 1 {
+				userPosts = append(userPosts, posts[i])
+			}
+		}
+		b, _ := json.Marshal(userPosts)
+		assert.Equal(t, string(b), w.Body.String())
+	}
+}
+
+func TestSellerCreatePostPassCase(t *testing.T) {
+	login := m.Login{
+		Username: "testadmin",
+		Password: "TestAdmin@123",
+	}
+	payload, _ := json.Marshal(login)
+	nr := httptest.NewRecorder()
+	req1, _ := http.NewRequest("POST", "/login", strings.NewReader(string(payload)))
+	req1.Header.Set("Content-Type", "application/json")
+	req1.Header.Set("credentials", "include")
+	router.ServeHTTP(nr, req1)
+	cookieValue := nr.Result().Header.Get("Set-Cookie")
+	if nr.Code == 200 {
+		post := m.Product{
+			UserId:      1,
+			Name:        "Office Chair",
+			Description: "All the wheels and functionality intact",
+			Location:    "3800 SW 34th Blvd",
+			Dimensions:  "10 x 10 x 10",
+			Weight:      110,
+			Age:         2,
+			Count:       1,
+		}
+		nr.Flush()
+		body, _ := json.Marshal(post)
+		req, _ := http.NewRequest("POST", "/create", strings.NewReader(string(body)))
+		req.Header.Set("Content-Type", "application/json")
+		req1.Header.Set("credentials", "include")
+		req.Header.Set("Cookie", cookieValue)
+		router.ServeHTTP(nr, req)
+		assert.Equal(t, 200, nr.Code)
+	}
+}
+
+func TestSellerCreatePostFailCase(t *testing.T) {
+	login := m.Login{
+		Username: "testadmin",
+		Password: "TestAdmin@123",
+	}
+	payload, _ := json.Marshal(login)
+	nr := httptest.NewRecorder()
+	req1, _ := http.NewRequest("POST", "/login", strings.NewReader(string(payload)))
+	req1.Header.Set("Content-Type", "application/json")
+	req1.Header.Set("credentials", "include")
+	router.ServeHTTP(nr, req1)
+	if nr.Code == 200 {
+		post := m.Product{
+			UserId:      1,
+			Name:        "Office Chair",
+			Description: "All the wheels and functionality intact",
+			Location:    "3800 SW 34th Blvd",
+			Dimensions:  "10 x 10 x 10",
+			Weight:      110,
+			Count:       1,
+		}
+		nr.Flush()
+		w := httptest.NewRecorder()
+		body, _ := json.Marshal(post)
+		req, _ := http.NewRequest("POST", "/create", strings.NewReader(string(body)))
+		req.Header.Set("Content-Type", "application/json")
+		req1.Header.Set("credentials", "include")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 400, w.Code)
+	}
+}
