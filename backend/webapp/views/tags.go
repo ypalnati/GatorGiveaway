45936d5ad014c4ad40e3419e14a0306bae042d36
Raghuveer Sharma Saripalli <rvs1604@gmail.com>
2022-04-04 16:35:15 -0500

    added get product by tags api and view method

--- backend/webapp/views/tags.go
+++ backend/webapp/views/tags.go
@@ -1,6 +1,15 @@
 package views
 
-import "strings"
+import (
+	"net/http"
+	"strings"
+	m "webapp/model"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+
+	"gorm.io/gorm"
+)
 
 // split tags string into list of tags
 func splitTagStringByHash(tags string) []string {
@@ -24,4 +33,39 @@ func removeDuplicateTags(tagList []string) string {
 func removeTagDuplicates(tags string) string {
 	tagList := splitTagStringByHash(tags)
 	return removeDuplicateTags(tagList)
+}
+
+func GetProductsByTags(db *gorm.DB) gin.HandlerFunc {
+
+	fn := func(c *gin.Context) {
+		// Get sessions
+		session := sessions.Default(c)
+
+		// Get user id from session
+		v := session.Get("uId")
+
+		// if there's no user id, return 400
+		if v == nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User is not logged in"})
+			return
+		}
+
+		//take tags from params
+		tags := c.Param("tags")
+
+		// get list of tags
+		tagList := splitTagStringByHash(tags)
+
+		// get all products
+		var products []m.Product
+		db.Find(&products)
+
+		numberOfTags := len(tagList)
+		for i := 0; i < numberOfTags; i++ {
+
+		}
+
+	}
+	// return the loginHandlerfunction
+	return gin.HandlerFunc(fn)
 }
