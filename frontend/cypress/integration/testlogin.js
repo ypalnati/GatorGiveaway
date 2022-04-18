08a1c333d6900e6d1604c7aa8b2d8f771a35a4e8
Yamini Palnati <yaminipalnati97@gmail.com>
2022-04-17 23:34:36 -0400

    CYPRESS for login successful attempt

--- frontend/cypress/integration/testlogin.js
+++ frontend/cypress/integration/testlogin.js
@@ -4,16 +4,20 @@ describe("if login is successful",()=>{
     it("test login",()=>{
         cy.visit("http://localhost:3000/");
        
-        cy.get("input[name=username]").type("GatorUser1").should('have.value','GatorUser1');
-        cy.get("input[name=password]").type("user1@123").should('have.value', 'user1@123');
+        cy.get("input[name=username]").type("a").should('have.value','a');
+        cy.get("input[name=password]").type("a").should('have.value', 'a');
+        cy.findAllByText('Sign In').focus().click()
+       
+        
 
 
     })
     });
 
-    describe("if sign exists",()=>{
-        it("test signin",()=>{
-            cy.visit("http://localhost:3000/");
-            cy.findAllByText("Sign In").should("exist");
-        })
-        });
+    // describe("if sign exists",()=>{
+    //     it("test signin",()=>{
+    //         cy.visit("http://localhost:3000/");
+    //         cy.findAllByText("Sign In").should("exist");
+    //     })
+    //     });
+
